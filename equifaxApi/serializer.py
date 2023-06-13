5f5b9a11388d14936a4db0ffedf396c8e0e5747c
AbhishekHappy <124674903+AbhishekHappy@users.noreply.github.com>
2023-06-13 15:11:35 +0530

    Delete equifaxApi directory

--- equifaxApi/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from equifaxApi.models import EquifaxAPI
-
-class EquifaxResponseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = EquifaxAPI
-        fields = '__all__'
