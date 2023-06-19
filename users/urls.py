17dd552a12251b85175e7ef9b17d38959945d764
AbhishekHappy <124674903+AbhishekHappy@users.noreply.github.com>
2023-06-19 12:24:45 +0530

    Add files via upload

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.UserListView.as_view()),
+    path('list/', views.UserListView.as_view()),
+    path('', views.LoginPage,name='Login')
 ]
--- users/views.py
+++ users/views.py
@@ -2,8 +2,36 @@ from rest_framework import generics
 
 from . import models
 from . import serializers
+from users.models import CustomUser
+from django.shortcuts import HttpResponse
+from django.shortcuts import render,redirect
+from django.contrib.auth.hashers import check_password
 
 
 class UserListView(generics.ListAPIView):
     queryset = models.CustomUser.objects.all()
     serializer_class = serializers.UserSerializer
+
+
+def LoginPage(request):
+    if request.method=='POST':
+        userlogin=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        print(userlogin,pass1) 
+        try:
+            # Check if a user with the given username and password exists
+            
+            user = CustomUser.objects.get(username=userlogin)
+            
+            if check_password(pass1, user.password):
+                print(user)
+
+            # login(request,user)
+            return redirect('list/')
+        except CustomUser.DoesNotExist:
+            # User does not exist or credentials are incorrect
+            return HttpResponse("Username or password is incorrect")
+
+        
+
+    return render(request,'login.html')
